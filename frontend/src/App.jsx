b09e6e130dfeab5ff3e5e343ca916ea1c2e48595
shikha singh <kumkum9151@gmail.com>
2026-09-07 00:21:48 +0530

    updating frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-//App
+
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Diagnostic from "./pages/Diagnostic";
